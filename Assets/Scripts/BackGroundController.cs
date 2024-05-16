f24c246962aa4431beba71873c4e24b29c2900de
yokoshi1123 <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2024-05-16 16:32:47 +0900

    背景1枚に変更

--- Assets/Scripts/BackGroundController.cs
+++ Assets/Scripts/BackGroundController.cs
@@ -4,42 +4,44 @@ using UnityEngine;
 
 public class BackGroundController : MonoBehaviour
 {
-    // ”wŒi‚Ì–‡”
-    private int spriteCount = 2;
-    // ”wŒi‚ª‰ñ‚è‚İ
-    private float rightOffset = 1.5f;
-    private float leftOffset = -0.5f;
+    //// ”wŒi‚Ì–‡”
+    //private int spriteCount = 2;
+    //// ”wŒi‚ª‰ñ‚è‚İ
+    //private float rightOffset = 1.5f;
+    //private float leftOffset = -0.5f;
 
-    private Transform bgTfm;
-    private SpriteRenderer mySpriteRndr;
+    //private Transform bgTfm;
+    //private SpriteRenderer mySpriteRndr;
     [SerializeField] private Transform player;
-    float width;
+    //float width;
 
     void Start()
     {
-        bgTfm = transform;
-        mySpriteRndr = GetComponent<SpriteRenderer>();
-        width = mySpriteRndr.bounds.size.x;
-        transform.position = new Vector2(transform.position.x, player.position.y + 1.5f);
+        //bgTfm = transform;
+        //mySpriteRndr = GetComponent<SpriteRenderer>();
+        //width = mySpriteRndr.bounds.size.x;
+        //transform.position = new Vector2(transform.position.x, player.position.y + 1.5f);
+        transform.position = new Vector2(player.position.x, player.position.y + 1.0f);
     }
 
 
     void Update()
     {
-        transform.position = new Vector2(transform.position.x, player.position.y + 1.5f);
-        // À•W•ÏŠ·
-        Vector3 myViewport = Camera.main.WorldToViewportPoint(bgTfm.position);
-        // Debug.Log(name + ":" + myViewport);
+        transform.position = new Vector2(player.position.x, player.position.y + 1.0f);
+        //transform.position = new Vector2(transform.position.x, player.position.y + 1.5f);
+        //// À•W•ÏŠ·
+        //Vector3 myViewport = Camera.main.WorldToViewportPoint(bgTfm.position);
+        //// Debug.Log(name + ":" + myViewport);
 
-        // ”wŒi‚Ì‰ñ‚è‚İ(ƒJƒƒ‰‚ªX²ƒvƒ‰ƒX•ûŒü‚ÉˆÚ“®)
-        if (myViewport.x < leftOffset)
-        {
-            bgTfm.position += Vector3.right * (width * spriteCount);
-        }
-        // ”wŒi‚Ì‰ñ‚è‚İ(ƒJƒƒ‰‚ªX²ƒ}ƒCƒiƒX•ûŒü‚ÉˆÚ“®)
-        else if (myViewport.x > rightOffset)
-        {
-            bgTfm.position -= Vector3.right * (width * spriteCount);
-        }
+        //// ”wŒi‚Ì‰ñ‚è‚İ(ƒJƒƒ‰‚ªX²ƒvƒ‰ƒX•ûŒü‚ÉˆÚ“®)
+        //if (myViewport.x < leftOffset)
+        //{
+        //    bgTfm.position += Vector3.right * (width * spriteCount);
+        //}
+        //// ”wŒi‚Ì‰ñ‚è‚İ(ƒJƒƒ‰‚ªX²ƒ}ƒCƒiƒX•ûŒü‚ÉˆÚ“®)
+        //else if (myViewport.x > rightOffset)
+        //{
+        //    bgTfm.position -= Vector3.right * (width * spriteCount);
+        //}
     }
 }
