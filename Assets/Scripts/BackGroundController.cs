b252ce8ac3e82ece51c84bb77c0ece2b4469ce10
yokoshi1123 <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2024-05-22 22:45:17 +0900

    x方向、y方向の端をBackGroundControllerのInspectorから設定できるように改善 Stage2Sceneはおおむね完成

--- Assets/Scripts/BackGroundController.cs
+++ Assets/Scripts/BackGroundController.cs
@@ -4,20 +4,23 @@ using UnityEngine;
 
 public class BackGroundController : MonoBehaviour
 {
-    [SerializeField] float yMin;
-    [SerializeField] float yMax;
+    [SerializeField] float xMin = 11f;
+    [SerializeField] float xMax = 133f;
+    [SerializeField] float yMin = 6f;
+    [SerializeField] float yMax = 10f;
     [SerializeField] private Transform player;
 
     void Start()
     {
-        transform.position = new Vector2(player.position.x, Mathf.Max(6.0f, player.position.y + 1.0f));
-        transform.position = new Vector2(transform.position.x, Mathf.Min(10.0f, transform.position.y));
+        Debug.Log(xMin + "," + xMax +"," + yMin + "," +yMax);
+        transform.position = new Vector2(Mathf.Max(xMin, player.position.x), Mathf.Max(yMin, player.position.y + 2.0f));
+        transform.position = new Vector2(Mathf.Min(xMax, transform.position.x), Mathf.Min(yMax, transform.position.y));
     }
 
 
     void Update()
     {
-        transform.position = new Vector2(player.position.x, Mathf.Max(6.0f, player.position.y + 1.0f));
-        transform.position = new Vector2(transform.position.x, Mathf.Min(10.0f, transform.position.y));
+        transform.position = new Vector2(Mathf.Max(xMin, player.position.x), Mathf.Max(yMin, player.position.y + 2.0f));
+        transform.position = new Vector2(Mathf.Min(xMax, transform.position.x), Mathf.Min(yMax, transform.position.y));
     }
 }
