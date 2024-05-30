3dab04a5ff3aa432b8c9922ac985c10b85697ab5
yokoshi1123 <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2024-05-30 16:15:19 +0900

    BGMとAudio Sourceの追加

---
+++ Assets/Scripts/BGMManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMManager : MonoBehaviour
+{
+    public bool DontDestroyEnabled = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        if (DontDestroyEnabled)
+        {
+            // Sceneを遷移してもオブジェクトが消えないようにする
+            DontDestroyOnLoad(this);
+        }
+    }
+}
