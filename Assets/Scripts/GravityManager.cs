29712ba47af59431f730c6586957f424f9f2e801
yokoshi1123 <yokota.koshi.g6@s.mail.nagoya-u.ac.jp>
2024-06-04 00:00:05 +0900

    重力の強さを「重力場：2.0G / 重」みたいな表記に変更 / 強さ変更はSキーか↓キーのみに / Stage2の難易度調整

--- Assets/Scripts/GravityManager.cs
+++ Assets/Scripts/GravityManager.cs
@@ -18,7 +18,10 @@ public class GravityManager : MonoBehaviour
     private float CAMERAZPOSITION = -20f;
     private GameObject destroyGF;
 
-    private int gScale = 2;
+    private int gScale = 1;
+
+    //private float mouseWheel = 1;
+    //private float mouthWheelDelta = 0;
 
     public TextMeshProUGUI gravityText;
 
@@ -26,6 +29,8 @@ public class GravityManager : MonoBehaviour
     {
         moveSpeed = M_SPEED;
         gravityScale = G_SCALE;
+        //gravityText.text = "重力場：--";
+        ChangeGravity();
     }
 
 
@@ -79,51 +84,33 @@ public class GravityManager : MonoBehaviour
             GetComponent<AudioSource>().Play();
         }
 
-        //if (Input.GetMouseButtonUp(0)) // マウスの左ボタンを離した時の座標を取得
-        //{
-        //    endMPosition = Input.mousePosition;
-        //    // スクリーン座標からワールド座標に変換
-        //    endMPosition = Camera.main.ScreenToWorldPoint(new Vector3(endMPosition.x, endMPosition.y, CAMERAZPOSITION));
-        //    // DebugLog("End:(" + endMPosition.x + ", " + endMPosition.y + ")");
-
-        //    if (startMPosition.x > endMPosition.x) // startMPosition.x > endMPosition.x なら値を入れ替え
-        //    {
-        //        // Debug.Log(startMPosition + ", " + endMPosition);
-        //        (startMPosition, endMPosition) = (endMPosition, startMPosition);
-        //        // Debug.Log(startMPosition + ", " + endMPosition);
-        //    }
-        //    if (startMPosition.y > endMPosition.y) // startMPosition.y > endMPosition.y なら値を入れ替え
-        //    {
-        //        // Debug.Log(startMPosition + ", " + endMPosition);
-        //        (startMPosition.y, endMPosition.y) = (endMPosition.y, startMPosition.y);
-        //        // Debug.Log(startMPosition + ", " + endMPosition);
-        //    }
-
-        //    // 既にGravityFieldのクローンがあれば削除
-        //    destroyGF = GameObject.FindWithTag("GravityField");
-        //    if (destroyGF != null) 
-        //    {
-        //        Destroy(destroyGF);
-        //    }
-        //    // GravityFieldのクローンを作成
-        //    GameObject gField = (GameObject)Instantiate(gravityField, (startMPosition + endMPosition) / 2, Quaternion.identity);
-        //    // 横幅をドラッグした幅に変更
-        //    gField.transform.localScale = new Vector2(Mathf.Abs(endMPosition.x - startMPosition.x), Mathf.Abs(endMPosition.y - startMPosition.y));
-
-        //}
-
         // 上下キーでgravityScaleの変更
-        if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
+        /*if (Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W))
         {
-            gScale = (gScale + 399999) % 4;
+            gScale = (gScale + 299999) % 3; // (gScale + 399999) % 4;
             ChangeGravity();
-        }
+        }*/
 
         if (Input.GetKeyDown(KeyCode.DownArrow) || Input.GetKeyDown(KeyCode.S))
         {
-            gScale = (gScale + 400001) % 4;
+            gScale = (gScale + 300001) % 3; // (gScale + 400001) % 4;
             ChangeGravity();
         }
+
+        /*if (Input.GetAxis("Mouse ScrollWheel") != 0)
+        {
+           *if (mouthWheelDelta != Input.GetAxis("Mouse ScrollWheel"))
+            {
+                Debug.Log("pre:" + mouthWheelDelta + ". now:" + Input.GetAxis("Mouse ScrollWheel"));
+                mouseWheel = (float)(int)mouseWheel;
+            }
+            mouthWheelDelta = Input.GetAxis("Mouse ScrollWheel");
+            mouseWheel += mouthWheelDelta;
+            Debug.Log(mouseWheel);
+            mouseWheel += Input.GetAxis("Mouse ScrollWheel");
+            gScale = ((int)mouseWheel + 30000) % 3;      
+            ChangeGravity();
+        }*/
     }
 
     void ChangeGravity()
@@ -137,23 +124,23 @@ public class GravityManager : MonoBehaviour
             case 0: // x(-1.0)
                 gravityScale = G_SCALE * (-1.0f);
                 moveSpeed = M_SPEED;
-                gravityText.text = "GRAVITY : -1.0";
+                gravityText.text = "重力場：-1G / 反転"; // "GRAVITY : -1.0";
                 break;
             case 1: // x0.5
                 gravityScale = G_SCALE * 0.5f;
                 moveSpeed = M_SPEED * 1.3f;
-                gravityText.text = "GRAVITY : 0.5";
+                gravityText.text = "重力場：0.5G / 軽"; // "GRAVITY : 0.5";
                 break;
-            case 2: // x1.0
-                gravityScale = G_SCALE;
-                moveSpeed = M_SPEED;
-                gravityText.text = "GRAVITY : 1.0";
+            case 2: // x1.0→x2.0
+                gravityScale = G_SCALE * 2.0f; // gravityScale = G_SCALE;
+                moveSpeed = M_SPEED * 0.7f; // moveSpeed = M_SPEED;
+                gravityText.text = "重力場：2.0G / 重"; // "GRAVITY : 1.0";
                 break;
-            case 3: // x2.0
-                gravityScale = G_SCALE * 2.0f;
-                moveSpeed = M_SPEED * 0.7f;
-                gravityText.text = "GRAVITY : 2.0";
-                break;
+            //case 3: // x2.0
+            //    gravityScale = G_SCALE * 2.0f;
+            //    moveSpeed = M_SPEED * 0.7f;
+            //    gravityText.text = "GRAVITY : 2.0";
+            //    break;
             default:
                 Debug.Log("ChangeGravityでエラー");
                 break;
